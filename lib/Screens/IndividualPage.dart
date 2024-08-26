83b988ace0aaec241e26749f04ce657bb594069e
Mostafa <mostafayku@gmail.com>
2024-08-26 23:36:18 +0300

    new

--- lib/Screens/IndividualPage.dart
+++ lib/Screens/IndividualPage.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
-//555555
+//5555555
 import 'package:chattapplication/CustomUI/Own/OwnFileDisplay.dart';
 import 'package:chattapplication/CustomUI/Reply/ReplyFileDisplay.dart';
 import 'package:chattapplication/CustomUI/Own/OwnImageCard.dart';
